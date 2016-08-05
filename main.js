7371425053ef234215720a3a697efdf8e03e7f51
carapia <gustavogomespc@yahoo.com.br>
2016-08-05 00:16:39 -0300

    adiciono base de dados

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-  
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
